819e6fa4463889bec04f48fc9e2a5de723cb8bcc
Dami <dmk9104@gmail.com>
2024-05-08 20:50:36 +0200

    add deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/ask-stoic-virtues/',
   plugins: [react()],
+  base: '/ask-stoic-virtues/',
 })
